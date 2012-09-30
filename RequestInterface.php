adc29d0cf708df64062fd318fd5c8f3e25bd8c49
Jonathan Hanson <jonathan@jonathan-hanson.org>
2012-09-29 18:33:49 -0700

    Added a RequestRootUri helper that provides a given request object's root uri as a string.

---
+++ RequestInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Groundhog\TemplateRenderer;
+
+interface RequestInterface
+{
+    /**
+     * Generates a normalized URI for the given path.
+     *
+     * @param string $path A path to use instead of the current one
+     *
+     * @return string The normalized URI for the path
+     *
+     * @api
+     */
+    public function getUriForPath($path);
+}
---
+++ RequestRootUri.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Groundhog\TemplateRenderer;
+
+class RequestRootUri implements ViewHelperInterface
+{
+    /**
+     * The http request object to which to delegate the URI construction
+     *
+     * @var RequestInterface
+     */
+    protected $http_request;
+
+    /**
+     * When set, this value is substituted in the URI's scheme for the next render().
+     *
+     * @var string|null
+     */
+    protected $scheme;
+
+    /**
+     * Set the http request object
+     *
+     * @param RequestInterface $http_request
+     */
+    public function __construct(RequestInterface $http_request)
+    {
+        $this->http_request = $http_request;
+    }
+
+    /**
+     * Set the sceme for the next render.
+     *
+     * Note that this scheme will get reset to the default after each render call.
+     *
+     * @param string $scheme
+     *
+     * @return \Common\RequestRootUri
+     */
+    public function setScheme($scheme)
+    {
+        $this->scheme = $scheme;
+
+        return $this;
+    }
+
+    /**
+     * Assemble a base path for this page request.
+     * For whatever reason, php doesn't include this concept natively.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $return = $this->http_request->getUriForPath('/');
+
+        if (!is_null($this->scheme)) {
+            $return = $this->scheme . substr($return, strpos($return, ':'));
+        }
+
+        $this->scheme = null;
+
+        return $return;
+    }
+}
