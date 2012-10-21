93136908af68dbc4107d517613f6ab4eb5968c60
Jonathan Hanson <jonathan@jonathan-hanson.org>
2012-10-21 12:56:20 -0700

    Reorganizing code to be CI-travis-able.  Reorganized to match my new standard for library directory structure.

---
+++ tests/Groundhog/TemplateRenderer/Tests/RequestRootUriTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Groundhog\TemplateRenderer\Tests;
+
+use Groundhog\TemplateRenderer\RequestRootUri;
+
+class RequestRootUriTest extends \PHPUnit_Framework_TestCase
+{
+}
---
+++ tests/Groundhog/TemplateRenderer/Tests/TemplateRendererPhpTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Groundhog\TemplateRenderer\Tests;
+
+use Groundhog\TemplateRenderer\TemplateRendererPhp;
+
+class TemplateRendererPhpTest extends \PHPUnit_Framework_TestCase
+{
+}
