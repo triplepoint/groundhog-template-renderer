7c0c390328dc9ae439701fc383f5cddbb2c9e66b
Jonathan Hanson <jonathan@jonathan-hanson.org>
2012-10-21 13:58:48 -0700

    Added decent unit test coverage

--- tests/Groundhog/TemplateRenderer/Tests/TemplateRendererPhpTest.php
+++ tests/Groundhog/TemplateRenderer/Tests/TemplateRendererPhpTest.php
@@ -6,4 +6,52 @@ use Groundhog\TemplateRenderer\TemplateRendererPhp;
 
 class TemplateRendererPhpTest extends \PHPUnit_Framework_TestCase
 {
+    public function testRendererCanLoadSimpleFileAndReturn()
+    {
+        $test_file        = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/simple_template.template';
+        $should_render_to = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/simple_template.template';
+
+        $renderer = new TemplateRendererPhp();
+
+        $output = $renderer->render($test_file);
+
+        $this->assertStringEqualsFile($should_render_to, $output);
+    }
+
+    public function testRendererRendersWithData()
+    {
+        $test_file        = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/data_template.template';
+        $should_render_to = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/data_template.output';
+
+        $renderer = new TemplateRendererPhp();
+
+        $output = $renderer->render($test_file, array('name' => 'Steve', 'age' => 29));
+
+        $this->assertStringEqualsFile($should_render_to, $output);
+    }
+
+    public function testRendererRendersWithSimplePartial()
+    {
+        $test_file        = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/partial_parent.template';
+        $should_render_to = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/partial_parent.output';
+
+        $renderer = new TemplateRendererPhp();
+
+        $output = $renderer->render($test_file);
+
+        $this->assertStringEqualsFile($should_render_to, $output);
+    }
+
+    public function testRendererRendersWithWrapper()
+    {
+        $test_file        = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/wrapped_template.template';
+        $should_render_to = 'tests/Groundhog/TemplateRenderer/Tests/test_docs/wrapped_template.output';
+
+        $renderer = new TemplateRendererPhp();
+
+        $output = $renderer->render($test_file);
+
+        $this->assertStringEqualsFile($should_render_to, $output);
+    }
+
 }
