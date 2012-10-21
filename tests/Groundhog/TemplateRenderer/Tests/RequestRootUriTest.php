c4ca938e6b73ff26def3f0d395e5c6db4c2869b2
Jonathan Hanson <jonathan@jonathan-hanson.org>
2012-10-21 13:32:43 -0700

    Added unit test for the RequestRootUri view helper.

--- tests/Groundhog/TemplateRenderer/Tests/RequestRootUriTest.php
+++ tests/Groundhog/TemplateRenderer/Tests/RequestRootUriTest.php
@@ -6,4 +6,34 @@ use Groundhog\TemplateRenderer\RequestRootUri;
 
 class RequestRootUriTest extends \PHPUnit_Framework_TestCase
 {
+    public function testGenerateUriWithoutSpecifiedScheme()
+    {
+        $mock_request = $this->getMock('\Groundhog\TemplateRenderer\RequestInterface');
+        $mock_request->expects($this->any())
+            ->method('getUriForPath')
+            ->will($this->returnValue('http://www.whatever.com/'));
+
+        $helper = new RequestRootUri($mock_request);
+
+        $uri = $helper->render();
+
+        $this->assertSame('http://www.whatever.com/', $uri);
+    }
+
+    public function testGenerateUriWithSpecifiedScheme()
+    {
+        $mock_request = $this->getMock('\Groundhog\TemplateRenderer\RequestInterface');
+        $mock_request->expects($this->any())
+            ->method('getUriForPath')
+            ->will($this->returnValue('http://www.whatever.com/'));
+
+        $helper = new RequestRootUri($mock_request);
+
+        $helper->setScheme('https');
+
+        $uri = $helper->render();
+
+        $this->assertSame('https://www.whatever.com/', $uri);
+    }
+
 }
